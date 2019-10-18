88f922b8df47e2313059343f910d342331697612
yulei <yulei@beyondcent.com>
2019-10-18 11:24:30 +0800

    first

---
+++ eureka-server/src/main/java/com/roger/springcloudFinchley/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.roger.springcloudFinchley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by admin on 2019/10/14.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
