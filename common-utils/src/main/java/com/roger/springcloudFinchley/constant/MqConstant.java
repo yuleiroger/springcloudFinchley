ef8ab9b77a911b3f7ebdd57099909a82a456447b
yulei <yulei@beyondcent.com>
2019-10-18 11:17:20 +0800

    first

---
+++ common-utils/src/main/java/com/roger/springcloudFinchley/constant/MqConstant.java
@@ -0,0 +1,9 @@
+package com.roger.springcloudFinchley.constant;
+
+/**
+ * Created by admin on 2019/10/11.
+ */
+public class MqConstant {
+    public final static String LogRabbitMq = "topic.logMessage";
+    public final static String LogExchange = "logExchange";
+}
