a666bed0e155027f6cd3a7ff0888f9729b6c811d
yulei <yulei@beyondcent.com>
2019-11-25 15:35:28 +0800

    update

--- zuul-server/src/main/java/com/roger/springcloudFinchley/ZuulApplication.java
+++ zuul-server/src/main/java/com/roger/springcloudFinchley/ZuulApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * Created by admin on 2019/10/10.
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
+@EnableRedisHttpSession
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
