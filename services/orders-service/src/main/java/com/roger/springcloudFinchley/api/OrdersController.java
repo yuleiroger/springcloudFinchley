ae057883310844cdefdb59f0bd57b1cf5cfa8be1
yulei <yulei@beyondcent.com>
2019-11-28 17:30:15 +0800

    update

--- services/orders-service/src/main/java/com/roger/springcloudFinchley/api/OrdersController.java
+++ services/orders-service/src/main/java/com/roger/springcloudFinchley/api/OrdersController.java
@@ -1,17 +1,26 @@
 package com.roger.springcloudFinchley.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Created by admin on 2019/10/22.
  */
 @RestController
+@Slf4j
 public class OrdersController {
+
+    @PostMapping(value = "/login")
+    public String login(@RequestBody String params) throws Exception{
+        log.info("login params is:{}", params);
+
+        return "success";
+    }
 
     @GetMapping(value = "setSession")
     public Object setSession(HttpServletRequest request){
