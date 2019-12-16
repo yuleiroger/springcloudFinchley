d19f87e46e09fd4507046357ed157479dc45e669
yulei <yulei@beyondcent.com>
2019-12-16 09:33:37 +0800

    auth

--- auth-service/src/main/java/com/roger/springcloudFinchley/config/WebSecurityConfig.java
+++ auth-service/src/main/java/com/roger/springcloudFinchley/config/WebSecurityConfig.java
@@ -1,16 +1,13 @@
 package com.roger.springcloudFinchley.config;
 
-import com.roger.springcloudFinchley.service.impl.UserServiceDetail;
+import com.roger.springcloudFinchley.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by admin on 2019/12/6.
@@ -21,22 +18,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private UserServiceDetail userServiceDetail;
+    private UserService userServiceDetail;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        //auth.inMemoryAuthentication().withUser("forezp").password("123456").roles("USER");
+        auth.userDetailsService(userServiceDetail);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .anyRequest().authenticated().and().csrf().disable();
+                .antMatchers("/css/**", "/index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .antMatchers("/blogs/**").hasRole("USER")
+                .and()
+                .formLogin().loginPage("/login").failureUrl("/login-error")
+                .and()
+                .exceptionHandling().accessDeniedPage("/401");
+        http.logout().logoutSuccessUrl("/");
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userServiceDetail).passwordEncoder(new BCryptPasswordEncoder());
-    }
 
-    @Override
-    @Bean
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
 }
