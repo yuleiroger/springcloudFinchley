8fcf569350c1eddbc6aa4c74079a6b0ed94aee13
yulei <yulei@beyondcent.com>
2019-12-20 13:04:56 +0800

    update

--- auth-service/src/main/java/com/roger/springcloudFinchley/AuthServiceApplication.java
+++ auth-service/src/main/java/com/roger/springcloudFinchley/AuthServiceApplication.java
@@ -2,13 +2,11 @@ package com.roger.springcloudFinchley;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by admin on 2019/12/6.
  */
 @SpringBootApplication
-@EnableEurekaClient
 public class AuthServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthServiceApplication.class, args);
